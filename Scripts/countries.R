f179c1efa987c15674f9b2e28983345b08fd09ad
Vadim Maletskii <wiscain@yandex.ru>
2023-06-07 13:28:24 +0200

    Attitude to Rus

--- Scripts/countries.R
+++ Scripts/countries.R
@@ -1,8 +1,10 @@
 library(readr)
 library(haven)
-natgov <- read_sav('Data_preproc/euro2018.sav') #Eurobarometer 90.2 (2018)
+euro2018<- read_sav('Data_preproc/euro2018.sav') #Eurobarometer 90.2 (2018)
 
-summary(natgov$qc5_6)
-hist(natgov$qc5_6)
-natgov$isocntry
+euro2018 <- subset(euro2018, !grepl("5", qc5_6))
+
+summary(euro2018$qc5_6)
+hist(euro2018$qc5_6)
+euro2018$isocntry
 
