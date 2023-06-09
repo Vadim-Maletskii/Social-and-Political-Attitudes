8d3f8abf40679ee2b71f0fd4b35dac002eb964f7
skorotkikh <s.korotkikh21@gmail.com>
2023-06-09 12:30:45 +0200

    Opinion variables reversed in nona_variables (1 ~ 4, 2 ~ 3, etc.)

--- Scripts/countries.R
+++ Scripts/countries.R
@@ -21,13 +21,77 @@ euro2018 <- euro2018 %>%
     qc5_6 == 4 ~ 1,
     TRUE ~ qc5_6
   ))
-
 # Independent + dependent variables
 all_variables <- euro %>% select(isocntry, qa3.3, qa3.15, qa3.16, qa6a_4, qa6a_12, qa8_4, qa8_8, qa8_9,
                                  qe2_1, qe2_2, qe2_3, qe2_4, qe2_5, qe2_6)
 nona_variables <- na.omit(all_variables)
 nona_variables <- filter(nona_variables, !(qa6a_4 %in% c(3,9)), qa6a_12 != 3, !(qa8_4 %in% c(5,9)),
                          qa8_8 != 5, qa8_9 != 5)
+
+nona_variables_reversed <- nona_variables %>%
+  mutate(qe2_1 = case_when(
+    qe2_1 == 1 ~ 4,
+    qe2_1 == 2 ~ 3,
+    qe2_1 == 3 ~ 2,
+    qe2_1 == 4 ~ 1,
+    TRUE ~ qe2_1
+  ),
+  qe2_2 = case_when(
+    qe2_2 == 1 ~ 4,
+    qe2_2 == 2 ~ 3,
+    qe2_2 == 3 ~ 2,
+    qe2_2 == 4 ~ 1,
+    TRUE ~ qe2_2
+  ),
+  qe2_3 = case_when(
+    qe2_3 == 1 ~ 4,
+    qe2_3 == 2 ~ 3,
+    qe2_3 == 3 ~ 2,
+    qe2_3 == 4 ~ 1,
+    TRUE ~ qe2_3
+  ),
+  qe2_4 = case_when(
+    qe2_4 == 1 ~ 4,
+    qe2_4 == 2 ~ 3,
+    qe2_4 == 3 ~ 2,
+    qe2_4 == 4 ~ 1,
+    TRUE ~ qe2_4
+  ),
+  qe2_5 = case_when(
+    qe2_5 == 1 ~ 4,
+    qe2_5 == 2 ~ 3,
+    qe2_5 == 3 ~ 2,
+    qe2_5 == 4 ~ 1,
+    TRUE ~ qe2_5
+  ),
+  qe2_6 = case_when(
+    qe2_6 == 1 ~ 4,
+    qe2_6 == 2 ~ 3,
+    qe2_6 == 3 ~ 2,
+    qe2_6 == 4 ~ 1,
+    TRUE ~ qe2_6
+  ),
+  qa8_4 = case_when(
+    qa8_4 == 1 ~ 4,
+    qa8_4 == 2 ~ 3,
+    qa8_4 == 3 ~ 2,
+    qa8_4 == 4 ~ 1,
+    TRUE ~ qa8_4
+  ),
+  qa8_8 = case_when(
+    qa8_8 == 1 ~ 4,
+    qa8_8 == 2 ~ 3,
+    qa8_8 == 3 ~ 2,
+    qa8_8 == 4 ~ 1,
+    TRUE ~ qa8_8
+  ),
+  qa8_9 = case_when(
+    qa8_9 == 1 ~ 4,
+    qa8_9 == 2 ~ 3,
+    qa8_9 == 3 ~ 2,
+    qa8_9 == 4 ~ 1,
+    TRUE ~ qa8_9
+  ))
 
 # Find unique values of isocntry in the dataset euro
 unique_countries <- unique(nona_variables$isocntry)
